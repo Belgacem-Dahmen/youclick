a680c0f628a466a089829766554d5ddfb823362a
belgacem-dahmen <belgacem.dahmen@gmail.com>
2025-02-01 16:53:41 +0100

    update response for wrong credentials

--- api/src/controllers/authController.js
+++ api/src/controllers/authController.js
@@ -75,7 +75,7 @@ const login = async (req, res) => {
     // Check if the password is correct
     const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
-      return res.status(400).json({ message: "Invalid password" });
+      return res.status(400).json({ message: "Wrong Credentials" });
     }
 
     // Generate a JWT token
